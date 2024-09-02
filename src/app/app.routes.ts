83b368a6d977f13fb322b0657dc791d11de5a438
darsruiz <darsruiz@gmail.com>
2024-09-02 21:13:21 +0200

    added 'about' text, fixed routing home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
+    { path: '**', pathMatch: 'full', component: HomeComponent },
 ];
