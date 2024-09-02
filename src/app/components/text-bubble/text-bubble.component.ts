f147a239b0171a993305fff59878bd0798c43b6b
darsruiz <darsruiz@gmail.com>
2024-09-02 19:55:28 +0200

    icons, text bubble, bg

---
+++ src/app/components/text-bubble/text-bubble.component.ts
@@ -0,0 +1,15 @@
+import { NgClass } from '@angular/common';
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'text-bubble',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './text-bubble.component.html',
+  styleUrl: './text-bubble.component.scss'
+})
+export class TextBubbleComponent {
+  isSendTextBuble = input<boolean>(false)
+  imageUrl = input<string>('https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp')
+  bubbleText = input<string>('Empty message')
+}
---
+++ src/app/interfaces/todo-entry.interface.ts
@@ -0,0 +1,6 @@
+export interface ITodoEntry {
+    userId: number
+    id: number
+    title: string
+    completed: boolean
+}
