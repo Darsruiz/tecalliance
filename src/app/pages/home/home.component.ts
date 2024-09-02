5727fe3b3519ab2d9c4474f22765230b6ac70ad5
darsruiz <darsruiz@gmail.com>
2024-09-02 21:30:48 +0200

    added max lenght, scroll to bottom

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, model, OnInit } from '@angular/core';
+import { Component, ElementRef, model, OnInit, ViewChild } from '@angular/core';
 import { ITodoEntry } from '../../interfaces/todo-entry.interface';
 import { TextBubbleComponent } from '../../components/text-bubble/text-bubble.component';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
@@ -15,6 +15,8 @@ import { EntriesService } from '../../services/entries.service';
   providers: [provideIcons({ heroPaperAirplane })],
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('chatContainer') private chatContainer: ElementRef | undefined;
+  
   robotImageUrl = 'https://www.flaticon.com/download/icon/3273762?icon_id=3273762&author=219&team=219&keyword=Robot&pack=3273628&style=1&style_id=914&format=png&color=%23000000&colored=2&size=512&selection=1&type=standard'
   message = model<string>('');
   entries: ITodoEntry[] = []
@@ -28,7 +30,7 @@ export class HomeComponent implements OnInit {
   fetchEntries() {
     this.entriesService.getEntries().subscribe({
       next: (data) => {
-        this.entries = data.filter(x => x.userId == this.currentUserId)
+        this.entries = data.filter(x => x.userId == this.currentUserId).sort((a, b) => a.id - b.id);
         console.log(data)
       }, error: () => {
         this.entries = [
@@ -44,6 +46,22 @@ export class HomeComponent implements OnInit {
   }
 
   addEntry() {
-    alert(this.message())
+    const nextId = this.entries.at(-1)?.id ?? 0;
+    const newEntry: ITodoEntry = {
+      completed: false,
+      id: nextId,
+      title: this.message(),
+      userId: this.currentUserId
+    }
+    this.message.update(x => x = '')
+    this.entries.push(newEntry)
+    this.scrollToBottom()
+  }
+
+  private scrollToBottom(): void {
+    try {
+      if (this.chatContainer)
+      this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
+    } catch(err) { }
   }
 }
