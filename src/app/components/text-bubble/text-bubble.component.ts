a997ff0e639c4aa75071796c543763879fefad4d
darsruiz <darsruiz@gmail.com>
2024-09-02 21:13:41 +0200

    added modify delete buttons

--- src/app/components/text-bubble/text-bubble.component.ts
+++ src/app/components/text-bubble/text-bubble.component.ts
@@ -1,15 +1,31 @@
 import { NgClass } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, input, model } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroPencilSquare, heroTrash } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'text-bubble',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, NgIconComponent],
   templateUrl: './text-bubble.component.html',
-  styleUrl: './text-bubble.component.scss'
+  styleUrl: './text-bubble.component.scss',
+  providers: [provideIcons({ heroPencilSquare, heroTrash })],
 })
 export class TextBubbleComponent {
   isUserTextBuble = input<boolean>(false)
+  isCompleted = model<boolean>(true)
   imageUrl = input<string>('https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp')
-  bubbleText = input<string>('Empty message')
+  bubbleText = model<string>('Empty message')
+
+  changeTaskCompletion() {
+    this.isCompleted.update(x => x = !x)
+  }
+
+  modifyText() {
+    alert('aaa')
+  }
+
+  removeBubble() {
+    
+  }
 }
